6a6f1c8ffb3d0a02617214e39befeaae89b12212
A1serious1programmer <114296963+A1serious1programmer@users.noreply.github.com>
2024-08-28 02:34:27 +0530

    loader

--- src/components/leaderboard/leaderb.jsx
+++ src/components/leaderboard/leaderb.jsx
@@ -35,7 +35,7 @@ const Leaderb = () => {
     return (
         <div className="bg-black min-h-screen max-w-[100%] text-white font-[Roboto] flex flex-col items-center overflow-x-hidden">
             <Header />
-            <div className='bg-black w-[80%] min-h-[70%]'>
+            <div className='bg-black w-[80%] h-[100%]'>
                 <div className="m-2 2xl:m-6 text-[40px] 2xl:text-[60px] font-cus2"> Leaderboard </div>
                 <div className='flex-col items-center m-[40px] mx-0 border-white border-2'>
                     <div className='flex m-[10px] md:m-[30px] border-0 md:border-b-2 border-white mb-16'>
@@ -50,7 +50,7 @@ const Leaderb = () => {
                     </div>
                     {board.map((ent, index) => {
                         return (<div key={index} className={`flex m-[20px] py-[2px] ${index%2 == 0 ? 'bg-gray-800' : ''}`}>
-                            <div className='hidden md:flex flex-[0.2] justify-center'>{index+1}</div>
+                            <div className='hidden md:flex flex-[0.2] justify-center'>{(no-1)*10+index+1}</div>
                             <div className='flex-[0.25] flex justify-center'>{ent.data.level*100}</div>
                             <div className='flex-[0.5] flex justify-center'>{ent.data.name}</div>
                             <div className='flex-[0.25] flex justify-center'>{ent.data.level}</div>
