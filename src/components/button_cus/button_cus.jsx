c1a079a92e3491efc3e1090287b2d6864aca8ca2
A1serious1programmer <114296963+A1serious1programmer@users.noreply.github.com>
2024-09-01 19:00:10 +0530

    reg

--- src/components/button_cus/button_cus.jsx
+++ src/components/button_cus/button_cus.jsx
@@ -1,15 +1,18 @@
-
+import { motion } from "framer-motion";
 
 const Button = (props) => {
     return (
-        <div className={`${props.width} ${props.height} bg-white ${props.border_width} ${props.text_size}
-        hover:cursor-pointer font-cus2`} 
-        style={{clipPath : "polygon(0.7rem 0, 100% 0, 100% calc(100% - 0.7rem), calc(100% - 0.7rem) 100%, 0 100%, 0 0.7rem)"}}>
-            <div className="bg-black p-0 flex w-[100%] h-[100%] justify-center items-center hover:bg-slate-800" 
+            <motion.button 
+            whileHover={{ scale: 1.01 }}
+            whileTap={{ scale: 0.95 }}
+            className={`${props.width} ${props.height} bg-white ${props.border_width} ${props.text_size}
+            hover:cursor-pointer font-cus2`} 
             style={{clipPath : "polygon(0.7rem 0, 100% 0, 100% calc(100% - 0.7rem), calc(100% - 0.7rem) 100%, 0 100%, 0 0.7rem)"}}>
-                {props.text}
-            </div>
-        </div>
+                <div className="bg-black p-0 flex w-[100%] h-[100%] justify-center items-center hover:bg-slate-800" 
+                style={{clipPath : "polygon(0.7rem 0, 100% 0, 100% calc(100% - 0.7rem), calc(100% - 0.7rem) 100%, 0 100%, 0 0.7rem)"}}>
+                    {props.text}
+                </div>
+        </motion.button>
     )
 }  
 
