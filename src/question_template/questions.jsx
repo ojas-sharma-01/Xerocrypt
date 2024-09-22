56f03ad5e37af274e8982a451e07779c821073dd
A1serious1programmer <114296963+A1serious1programmer@users.noreply.github.com>
2024-09-22 15:47:58 +0530

    ques

--- src/question_template/questions.jsx
+++ src/question_template/questions.jsx
@@ -1,6 +1,14 @@
 const questions = [
     {q_no:1, question:"what is ur name"},
     {q_no:2, question:"hilakd"},
+    {q_no:3, question:"hilakd"},
+    {q_no:4, question:"hilakd"},
+    {q_no:5, question:"hilakd"},
+    {q_no:6, question:"hilakd"},
+    {q_no:7, question:"hilakd"},
+    {q_no:8, question:"hilakd"},
+    {q_no:9, question:"hilakd"},
+    {q_no:10, question:"hilakd"}
 ]
 
 export default questions;
